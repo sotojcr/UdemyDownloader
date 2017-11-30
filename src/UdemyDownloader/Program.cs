f0e2047556e0f38b704e8632c6ca9d9ff14ac999
Marcel Lambacher <web@marcel-lambacher.de>
2017-11-30 17:34:24 +0100

    Initial project creation

---
+++ src/UdemyDownloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UdemyDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
